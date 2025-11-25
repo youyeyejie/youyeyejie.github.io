a70987dde6790b0f2dc7c5d24dfe181c02a8fa6e
youyeyejie <youyeyejie@outlook.com>
2025-11-26 00:20:05 +0800

    Site updated: 2025-11-26 00:20:02

--- js/RightMenu.js
+++ js/RightMenu.js
@@ -244,19 +244,29 @@ function toggleColorMode() {
 
 // 切换全屏背景功能
 function toggleBackgroundMode() {
-    const background = localStorage.getItem('BackgroundMode');
-    if (background === 'false' || !background) { // 当前非全屏背景，切换为全屏背景
-        document.querySelector('#web_bg').style.display = 'block';
+    const BackgroundMode = localStorage.getItem('BackgroundMode');
+    if (BackgroundMode === 'false' || !BackgroundMode) {
+        document.querySelector('#web_bg').style.backgroundImage = `${document.querySelector('.banner').style.background.split(' ')[0]}`;
         document.querySelector("#banner").style.background = 'url()';
         document.querySelector("#banner .mask").style.backgroundColor = 'rgba(0,0,0,0)';
         document.getElementById('toggle-background-mode-icon').className = "fa-solid fa-toggle-on";
+        ['#toc', '.category-list'].forEach(selector => {
+            if (document.querySelector(selector)) {
+                document.querySelector(selector).style.backgroundColor = "var(--board-bg-color)";
+            }
+        });
         localStorage.setItem('BackgroundMode', 'true');
     }
-    else { // 当前为全屏背景，切换为非全屏背景
-        document.querySelector('#web_bg').style.display = 'none';
+    else {
         document.querySelector("#banner").style.background = document.querySelector('#web_bg').style.backgroundImage + " center center / cover no-repeat";
+        document.querySelector('#web_bg').style.backgroundImage = 'url()';
         document.querySelector("#banner .mask").style.backgroundColor = 'rgba(0,0,0,0.3)';
         document.getElementById('toggle-background-mode-icon').className = "fa-solid fa-toggle-off";
+        ['#toc', '.category-list'].forEach(selector => {
+            if (document.querySelector(selector)) {
+                document.querySelector(selector).style.removeProperty('background-color');
+            }
+        });
         localStorage.setItem('BackgroundMode', 'false');
     }
 }
