c807dedad573b07436cccb84976ac5b4ff465fed
youyeyejie <youyeyejie@outlook.com>
2025-11-25 13:19:00 +0800

    Site updated: 2025-11-25 13:18:57

--- js/Background.js
+++ js/Background.js
@@ -2,11 +2,12 @@ document.querySelector('#web_bg').style.backgroundImage = `${document.querySelec
 document.querySelector('#web_bg').style.display = 'block';
 document.querySelector("#banner").style.background = 'url()'
 document.querySelector("#banner .mask").style.backgroundColor = 'rgba(0,0,0,0)'
-document.getElementById('toggle-background-script-icon').className = "fa-solid fa-toggle-on";
+document.getElementById('toggle-background-mode-icon').className = "fa-solid fa-toggle-on";
 
-if (localStorage.getItem('BackgroundScript') === 'false' || !localStorage.getItem('BackgroundScript')) {
+if (localStorage.getItem('BackgroundMode') === 'false' || !localStorage.getItem('BackgroundMode') || window.innerWidth < 768) {
     document.querySelector('#web_bg').style.display = 'none';
     document.querySelector("#banner").style.background = document.querySelector('#web_bg').style.backgroundImage + " center center / cover no-repeat";
     document.querySelector("#banner .mask").style.backgroundColor = 'rgba(0,0,0,0.3)';
-    document.getElementById('toggle-background-script-icon').className = "fa-solid fa-toggle-off";
+    document.getElementById('toggle-background-mode-icon').className = "fa-solid fa-toggle-off";
+    localStorage.setItem('BackgroundMode', 'false');
 }
